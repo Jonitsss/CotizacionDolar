174db6fbe38ef86dc2bd088125caf4a7f4cc12ea
Jonits <Jonathan0gab24@gmail.com>
2023-08-18 18:45:16 -0300

    Conversion de pesos a dolares

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
-const input1 = document.getElementById("input1");
-const input2 = document.getElementById("input2");
-const button = document.getElementById("btn");
-const form = document.querySelector("form");
-const txt = document.getElementById("txt");
+const input1 = document.getElementById("input1")
+const input2 = document.getElementById("input2")
+const button = document.getElementById("btn")
+const form = document.querySelector("form")
+const txt = document.getElementById("txt")
 
 const url = 'https://api.bluelytics.com.ar/v2/latest';
 
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        const blueDollarValue =  data.blue.value_avg;
+        const blueDollarValue =  data.blue.value_avg
         txt.innerText = 'El valor actual del Dólar es : ' + blueDollarValue
         form.addEventListener("submit", async (e) => {
-            e.preventDefault();
+            e.preventDefault()
 
-            button.setAttribute('aria-busy', 'true');
+            button.setAttribute('aria-busy', 'true')
             
-            const dólares = parseFloat(input1.value);
-            const pesos = dólares * blueDollarValue;
+            const dólares = parseFloat(input1.value)
+            const pesos = dólares * blueDollarValue
             
-            await new Promise(resolve => setTimeout(resolve, 100));
+            await new Promise(resolve => setTimeout(resolve, 100))
             
-            input2.value = pesos;
+            input2.value = pesos
             
-            button.setAttribute('aria-busy', 'false');
-        });
+            button.setAttribute('aria-busy', 'false')
+        })
+        form.addEventListener("submit", async (e) => {
+            e.preventDefault()
+
+            button.setAttribute('aria-busy', 'true')
+            
+            const pesos = parseFloat(input2.value);
+            const dólares = pesos / blueDollarValue
+            
+            await new Promise(resolve => setTimeout(resolve, 100))
+            
+            input1.value = dólares
+            
+            button.setAttribute('aria-busy', 'false')
+        })
     })
     .catch(error => {
-        console.error('Ocurrió un error:', error);
-    });
+        console.error('Ocurrió un error:', error)
+    })
 
